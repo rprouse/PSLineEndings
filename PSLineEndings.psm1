a2a188477447a97955ac326c49e01f96af11599c
Rob Prouse <rob@prouse.org>
2015-12-15 11:28:39 -0500

    Fixes #3 - Added help for the commands

--- PSLineEndings.psm1
+++ PSLineEndings.psm1
@@ -2,15 +2,63 @@
 $unix2dos = Join-Path -Path $bin -ChildPath unix2dos.exe
 $dos2unix = Join-Path -Path $bin -ChildPath dos2unix.exe
 
-function ConvertTo-Dos ($Filter, [switch]$Recurse) {
-    Get-ChildItem -File @psBoundParameters | ForEach-Object { 
-        $return = Start-Process -FilePath "`"$unix2dos`"" -ArgumentList "`"$($_.FullName)`"" -NoNewWindow -Wait -PassThru
+<#
+.SYNOPSIS
+Converts a files line endings from Unix to Dos
+
+.DESCRIPTION
+Changes all line endings in the file from LF to CRLF
+
+.PARAMETER Filter
+The file filter
+
+.PARAMETER Recurse
+Whether or not to recurse through sub-directories
+
+.EXAMPLE
+ConvertTo-Dos *.js
+#>
+function ConvertTo-Dos { 
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$True,Position=0)]
+        [string]$Filter, 
+        [switch]$Recurse
+    ) 
+    PROCESS {
+        Get-ChildItem -File @psBoundParameters | ForEach-Object { 
+            $return = Start-Process -FilePath "`"$unix2dos`"" -ArgumentList "`"$($_.FullName)`"" -NoNewWindow -Wait -PassThru
+        }
     }
 }
 
-function ConvertTo-Unix ($Filter, [switch]$Recurse) {
-    Get-ChildItem -File @psBoundParameters | ForEach-Object { 
-        $return = Start-Process -FilePath "`"$dos2unix`"" -ArgumentList "`"$($_.FullName)`"" -NoNewWindow -Wait -PassThru
+<#
+.SYNOPSIS
+Converts a files line endings from Dos to Unix
+
+.DESCRIPTION
+Changes all line endings in the file from CRLF to LF
+
+.PARAMETER Filter
+The file filter
+
+.PARAMETER Recurse
+Whether or not to recurse through sub-directories
+
+.EXAMPLE
+ConvertTo-Unix *.js
+#>
+function ConvertTo-Unix { 
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$True,Position=0)]
+        [string]$Filter, 
+        [switch]$Recurse
+    ) 
+    PROCESS {
+        Get-ChildItem -File @psBoundParameters | ForEach-Object { 
+            $return = Start-Process -FilePath "`"$dos2unix`"" -ArgumentList "`"$($_.FullName)`"" -NoNewWindow -Wait -PassThru
+        }
     }
 }
 
